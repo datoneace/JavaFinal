097cb1e3b760e2eeff69cc1c9ce5c77d50ffca29
datoneace <ah308508@students.davenport.k12.ia.us>
2017-05-05 13:21:00 -0500

    changes

---
+++ Final/src/thefinal/Girl.java
@@ -0,0 +1,11 @@
+package thefinal;
+
+public class Girl {
+
+	public static void girl() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("The average of the array is:girl");
+	}
+
+}
