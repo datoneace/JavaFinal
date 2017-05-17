6e848ff10e016405c07665e4d3302d02eff97590
Alex352 <alex.mcclure352@gmail.com>
2017-05-17 12:40:01 -0500

    update

--- Final/src/thefinal/Shelter.java
+++ Final/src/thefinal/Shelter.java
@@ -23,11 +23,11 @@ public class Shelter {
 	           do {
 	              DiceGame.roll(); 
 	               rollCount++;
-	               System.out.println("To quit type Quit otherwise press play to play again.");
+	               System.out.println("To quit type No otherwise type Yes to play again.");
 	               quit = input.nextLine();
-	           } while ("Quit".equals(quit) ||"quit".equals(quit));
+	           } while ("Yes".equals(quit) ||"yes".equals(quit));
 	         
-	        System.out.println("'After " + rollCount +" a few rolls you finally win.'");
+	        System.out.println("'After " + rollCount +" rolls you finally are bored and want to move on.'");
 	        System.out.println("'I should go outside'");
 		}
 		else
@@ -39,10 +39,12 @@ public class Shelter {
 	static void leaveShelter()
 	{     
 		int choice = 0;
-		int hunt, random = 0;
+		int hunt;
 		int fire= 0;
 		int total = 0;
-		int tryAgain; 
+		int tryAgain;
+		int thisMany = 0;
+		int missedCount = 0;
 		System.out.println("'You realize you are very hungry, what do you want to do? Go hunt(1) or make fire(2)?'");
 		choice = input.nextInt();
 		if(choice == 1)
@@ -52,12 +54,21 @@ public class Shelter {
 			do{
 			System.out.println("Pick a number between 1 and 12 to see if you hit.");
 			hunt = input.nextInt();
-			rollHunt(random);
-			if(hunt == random)
+			rollHunt(total);
+			if(hunt == total)
 			{
-				System.out.println("'You set up the trap and wait you see one. You got a rabbit!'");
+				System.out.println("'You set up the trap and wait, then you see one. You got a rabbit! MMMMmhhh'");
 			}
-			else 
+			
+			missedCount++;
+			System.out.println("You have missed the rabbit " + missedCount + " times if you get to 5 you die of starvation.");
+			if(missedCount == 5)
+			{
+				System.out.println("You have starved to death. Good luck next time.");
+				GameOver.displayOver();
+				MainClass.main(null);
+				
+			} 
 			System.out.println("'You missed the rabbit. Maybe a trap would be better. Try again? Quit 999'");
 			tryAgain = input.nextInt();
 			}while(tryAgain != 999);
@@ -66,7 +77,9 @@ public class Shelter {
 		if(choice == 2)
 		{
 			System.out.println("'You go out to get sticks for the fire.'");
-			System.out.println("''");
+			System.out.println("' x '");
+			System.out.println("'xxx'");
+			System.out.println("Nice fire dude!");
 		}
 	
 	}
